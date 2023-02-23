382b51d4557629d646c705b514027703e22a5984
ichxrrly <82231195+ichxrrly@users.noreply.github.com>
2023-02-24 00:30:12 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+
+//hamburger menu//
+
+
+function toggleMenu() {
+  var x = document.getElementById("links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+
+//slide up transition//
+
+function reveal() {
+  var reveals = document.querySelectorAll(".section2");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 170;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
